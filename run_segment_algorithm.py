19ec03b79276833ff25691a3707ad31f270b2a03
LeahDZuckerman <LeahDZuckerman@gmail.com>
2023-02-24 17:50:02 -0700

    add script for segmenting with prior algorithm

--- run_segment_algorithm.py
+++ run_segment_algorithm.py
@@ -4,20 +4,44 @@ import astropy.io.fits as fits
 import funclib
 import numpy as np
 
-# Get data
-dir = os.getcwd()+"/Data/DKIST_gband_series_183653/"
-files = os.listdir(dir)
-print(files)
-for file in files:
-    if not file.startswith('seg_'):
-        print('Input file '+str(file))
-        if os.path.exists(dir+'seg_'+str(file)):
-            print('     Segmented data already exists')
-        else:
-            data = fits.open(dir+file)[1].data # for files in DKIST_gband_series_183653, first HDU is just header
-            seg_data = funclib.segment_array(data, resolution=0.016)
-            seg_hdu = fits.PrimaryHDU(seg_data)
-            raw_hdu = fits.ImageHDU(data)
-            hdu = fits.HDUList([seg_hdu, raw_hdu])
-            print('     Writing segmented data to'+str(dir)+'seg_'+str(file))
-            hdu.writeto(dir+'seg_'+str(file), overwrite=True)
+# # Run on 'new' DKIST data - cut down to 1/9 the size
+# dir = os.getcwd()+"/Data/DKIST_gband_series_183653/"
+# files = os.listdir(dir)
+# print(files)
+# for file in files:
+#     if not (file.startswith('seg_') or file.startswith('SEG_')):
+#         print('Input file '+str(file))
+#         if os.path.exists(dir+'SEG_'+str(file)):
+#             print('     Segmented data already exists')
+#         if '53_20' in file: # still not sure what the error is for this file
+#             print('     skipping file ')
+#         else:
+#             data = fits.open(dir+file)[1].data # for files in DKIST_gband_series_183653, first HDU is just header
+#             data = data[0:int(np.shape(data)[0]/3), 0:int(np.shape(data)[1]/3)] # cut data by a factor of 9
+#             header = fits.open(dir+file)[1].header
+#             seg_data = funclib.segment_array(data, resolution=header['cdelt1'], mark_dim_centers=True), #resolution=0.016)
+#             seg_hdu = fits.PrimaryHDU(seg_data)
+#             raw_hdu = fits.ImageHDU(data)
+#             hdu = fits.HDUList([seg_hdu, raw_hdu])
+#             print('     Writing segmented data to SEG_'+str(file))
+#             hdu.writeto(dir+'SEG_'+str(file), overwrite=True) # hdu.writeto(str(dir)+'SEG_'+str(file), overwrite=True)
+
+# Run on 'old' DKIST data
+file = os.getcwd()+"/Data/DKIST_example.fits"
+data = fits.open(file)[0].data 
+header = fits.open(file)[0].header
+seg_data = funclib.segment_array(data, resolution=0.016, mark_dim_centers=True, mark_BP=True), #resolution=0.016)
+seg_hdu = fits.PrimaryHDU(seg_data)
+raw_hdu = fits.ImageHDU(data)
+hdu = fits.HDUList([seg_hdu, raw_hdu])
+hdu.writeto('Data/DKIST_solarseg_output1.fits', overwrite=True)
+
+# # Run on 'old' DKIST data, but without BPs and DMs
+# file = os.getcwd()+"/Data/DKIST_example.fits"
+# data = fits.open(file)[0].data 
+# header = fits.open(file)[0].header
+# seg_data = funclib.segment_array(data, resolution=0.016, mark_dim_centers=False, mark_BP=False), #resolution=0.016)
+# seg_hdu = fits.PrimaryHDU(seg_data)
+# raw_hdu = fits.ImageHDU(data)
+# hdu = fits.HDUList([seg_hdu, raw_hdu])
+# hdu.writeto('Data/DKIST_solarseg_output_noBPDMs.fits', overwrite=True)
