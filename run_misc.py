efa85e52ebfb9357379db1e5dee12c1d4f141f56
LeahDZuckerman <LeahDZuckerman@gmail.com>
2024-08-12 11:59:00 -0600

    find timeseries matches

--- run_misc.py
+++ run_misc.py
@@ -1,5 +1,7 @@
 import argparse
 import run_WNet
+import numpy as np
+import os, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-task", "--task", type=str, required=True)
@@ -54,12 +56,12 @@ if args.task == 'find_image':
     Go through MURaM test set to find image idxs corresponding to MURaM time-series test set image 58
     '''
     
-    ts_im_58 = np.load('../WNet_runs/exp53nm/test_preds_MURaM/x_58.npy')[0] # should really not be sacing test set with each model but oh well
+    ts_im_58 = np.load('../WNet_runs/exp34nm/test_preds_MURaM/x_58.npy')[0] # should really not be sacing test set with each model but oh well
     all_ims = [f for f in os.listdir('../WNet_runs/exp29nm/test_preds_MURaM') if 'x_' in f]
-    print(f'Searching through {len(ims)} images for match')
+    print(f'Searching through {len(all_ims)} images for match')
     for f in all_ims:
-        im = np.load(f'../WNet_runs/exp29nm/test_preds_MURaM/{f}')
-        if (np.sum(im)==np.sum(ts_im_58)):
-            print(f'Image {im} seems to match')
+        im = np.load(f'../WNet_runs/exp29nm/test_preds_MURaM/{f}')[0]
+        if np.all(im == ts_im_58):
+            print(f'Image {f} seems to match')
     print('DONE')
     
